8536271fed5172d3303415784b858a65110c5341
TaLu-DevNoob <talu21.02@gmail.com>
2021-10-27 19:40:22 +0700

    Correct the variables

--- BST.cpp
+++ BST.cpp
@@ -1,41 +1,45 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-class Node{
-    public:
-        int data;
-        Node* pleft;
-        Node* pright;
+class Node
+{
+public:
+    int data;
+    Node *pLeft;
+    Node *pRight;
 };
 
-Node* CreateNode(int x)
+Node *CreateNode(int x)
 {
-    Node* p = new Node;
+    Node *p = new Node;
     p->data = x;
-    p->pleft = NULL;
-    p->pright = NULL;
+    p->pLeft = NULL;
+    p->pRight = NULL;
     return p;
 }
-void addToNode(int x, Node* key)
+void addToNode(int x, Node *key)
 {
-    if (x < key->data){
-        if (key->pleft == NULL)
-            key->pleft = CreateNode(x);
-        else 
-            addToNode(x,key->pleft);
+    if (x < key->data)
+    {
+        if (key->pLeft == NULL)
+            key->pLeft = CreateNode(x);
+        else
+            addToNode(x, key->pLeft);
     }
-    else{
-        if (key->pright == NULL)
-            key->pright = CreateNode(x);
-        else 
-            addToNode(x, key->pright);
+    else
+    {
+        if (key->pRight == NULL)
+            key->pRight = CreateNode(x);
+        else
+            addToNode(x, key->pRight);
     }
 }
 
-class BST{
-    public:
-        Node* root = NULL;
+class BST
+{
+public:
+    Node *root = NULL;
 };
 
 void addToBST(int x, BST &tree)
@@ -45,13 +49,13 @@ void addToBST(int x, BST &tree)
     else
         addToNode(x, tree.root);
 }
-int getHeight(Node* p)
+int getHeight(Node *p)
 {
     int hL = 0, hR = 0;
-    if (p->pleft != NULL)
-        hL = getHeight(p->pleft);
-    if (p->pright != NULL)
-        hR = getHeight(p->pright);
+    if (p->pLeft != NULL)
+        hL = getHeight(p->pLeft);
+    if (p->pRight != NULL)
+        hR = getHeight(p->pRight);
     return 1 + max(hL, hR);
 }
 int main()
@@ -59,7 +63,8 @@ int main()
     int n;
     cin >> n;
     BST tree;
-    for(int i = 0; i < n; i ++){
+    for (int i = 0; i < n; i++)
+    {
         int temp;
         cin >> temp;
         addToBST(temp, tree);
